7e1fcf60e9b6f5fca6964f67b057f5aa42811d8c
Thompouce1 <chalte.thomas57@gmail.com>
2017-11-19 23:37:41 +0100

    Modifications

--- App/AppTracker/AppTracker/AppTracker/MainPage.xaml.cs
+++ App/AppTracker/AppTracker/AppTracker/MainPage.xaml.cs
@@ -13,12 +13,12 @@ namespace AppTracker
 		public MainPage()
 		{
             var apps = Android.App.Application.Context.PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
-            var app1 = apps[0].LoadLabel(Android.App.Application.Context.PackageManager);
-            var app2 = apps[1].LoadLabel(Android.App.Application.Context.PackageManager);
-            var app3 = apps[2].LoadLabel(Android.App.Application.Context.PackageManager);
-            var app4 = apps[3].LoadLabel(Android.App.Application.Context.PackageManager);
-            var app5 = apps[4].LoadLabel(Android.App.Application.Context.PackageManager);
-            var app6 = apps[5].LoadLabel(Android.App.Application.Context.PackageManager);
+            var app1 = apps[0].PackageName.Split(".");
+            var app2 = apps[1].PackageName;
+            var app3 = apps[2].PackageName;
+            var app4 = apps[3].PackageName;
+            var app5 = apps[4].PackageName;
+            var app6 = apps[5].PackageName;
 
             var iconapp1 = apps[0].LoadLabel(Android.App.Application.Context.PackageManager);
             var iconapp2 = apps[1].LoadLabel(Android.App.Application.Context.PackageManager);
@@ -27,12 +27,12 @@ namespace AppTracker
             var iconapp5 = apps[4].LoadLabel(Android.App.Application.Context.PackageManager);
             var iconapp6 = apps[5].LoadLabel(Android.App.Application.Context.PackageManager);
 
-            Console.Write(app1);
-            Console.Write(app2);
-            Console.Write(app3);
-            Console.Write(app4);
-            Console.Write(app5);
-            Console.Write(app6);
+            Console.WriteLine(app1);
+            Console.WriteLine(app2);
+            Console.WriteLine(app3);
+            Console.WriteLine(app4);
+            Console.WriteLine(app5);
+            Console.WriteLine(app6);
 
             InitializeComponent();
 		}
